49b2063bf0472ba04966522575119849c6ed9783
summerthread <summerthread@163.com>
2020-05-27 18:02:29 +0800

    change

---
+++ DDDAdSdk/Classes/DDDAdSdk.framework/Headers/DDDNativeExpressAdView.h
@@ -0,0 +1,35 @@
+//
+//  DDDNativeExpressAdView.h
+//  DDDDemo
+//
+//  Created by mu on 2020/3/4.
+//  Copyright © 2020 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DDDNativeExpressAdView : UIView
+
+
+@property (nonatomic, assign, readonly) BOOL isReady;
+
+
+@property (nonatomic, copy, readonly) NSDictionary *mediaExt;
+
+
+@property (nonatomic, weak) UIViewController *rootViewController;
+
+@property (nonatomic,assign) id object;
+
+
+- (void)configDataView ;
+/**
+ 必须调用  -- 在调用configDataView后调用
+ */
+- (void)render;
+
+@end
+
+NS_ASSUME_NONNULL_END
